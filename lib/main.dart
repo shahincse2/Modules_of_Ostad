84fe01712906b4fc756d8889cf5a1d5a3ef42c0d
shahincse2 <shahinalam.cse2@gmail.com>
2024-08-17 12:16:10 +0600

    Floating action, Drawer, BottomNavigation, Column with Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,436 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Home extends StatelessWidget {
+  const Home({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  MySnackBar(message, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text(
+          'Floating Action',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueAccent[100],
+        actions: [
+          IconButton(
+              onPressed: () {
+                MySnackBar('Add a photo', context);
+              },
+              icon: const Icon(
+                Icons.add_a_photo,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {
+                MySnackBar('Add a comment', context);
+              },
+              icon: const Icon(
+                Icons.comment_sharp,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {
+                MySnackBar('See notifications', context);
+              },
+              icon: const Icon(
+                Icons.notifications_active,
+                color: Colors.white,
+              )),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: const Body(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          MySnackBar('You Clicked on Float Button', context);
+        },
+        elevation: 15,
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        currentIndex: 0,
+        selectedItemColor: Colors.deepOrangeAccent,
+        unselectedItemColor: Colors.black26,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(
+            color: Colors.black26, fontSize: 15, fontWeight: FontWeight.bold),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.list_alt_sharp), label: 'Transactions'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.people_outlined), label: 'People'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.menu_open_sharp), label: 'My Nagad'),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: const EdgeInsets.all(0),
+              child: UserAccountsDrawerHeader(
+                decoration: const BoxDecoration(color: Colors.deepOrangeAccent),
+                accountName: const Text('Md. Shahin Alam'),
+                accountEmail: const Text('01742833791'),
+                currentAccountPicture: Image.asset("assets/images/man.png"),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ListTile(
+              leading: const Icon(
+                Icons.sort_by_alpha,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: const Text(
+                'Language',
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: () {
+                MySnackBar('Select your language', context);
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.person_search,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'Account Type',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.auto_graph_rounded,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'I want Profit',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.lock,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'Change PIN',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.cell_tower,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'Change Mobile Operator',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.event_note,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'Re-submit KYC',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.security,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'Trusted Merchants',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.privacy_tip,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'Privacy Policy',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_mark,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'FAQ',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.place_outlined,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'Store Locator',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.merge,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'About',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.facebook,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'Nagad Page',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.browse_gallery_rounded,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'Website',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.logout_sharp,
+                color: Colors.deepOrangeAccent,
+              ),
+              title: Text(
+                'Sign Out',
+                style: TextStyle(fontSize: 18),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(left: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          verticalDirection: VerticalDirection.down,
+          children: [
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            TextButton(onPressed: () {}, child: Text('data')),
+            TextButton(onPressed: () {}, child: Text('Button')),
+            TextButton(onPressed: () {}, child: Text('Click Me')),
+            TextButton(onPressed: () {}, child: Text('Log Out')),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me on button");
+                },
+                child: const Text(
+                  'Click Me',
+                )),
+            ElevatedButton(
+                autofocus: true,
+                onPressed: () {
+                  print("Clicked me");
+                },
+                child: const Text(
+                  'Click Me on the button',
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// class BottomNavigation extends StatelessWidget {
+//   const BottomNavigation({super.key});
+//
+//   MySnackBar(message, context) {
+//     return ScaffoldMessenger.of(context)
+//         .showSnackBar(SnackBar(content: Text(message)));
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       bottomNavigationBar: BottomNavigationBar(
+//           items: [
+//             BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
+//             BottomNavigationBarItem(icon: Icon(Icons.people_alt_sharp), label: 'People'),
+//             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Stories'),
+//           ],
+//       ),
+//     );
+//   }
+// }
