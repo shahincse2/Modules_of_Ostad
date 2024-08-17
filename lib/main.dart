c0048cc46b827af443e7b8d4d4bebe97ff534067
shahincse2 <shahinalam.cse2@gmail.com>
2024-08-17 12:20:34 +0600

    ListView, GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> friendList = [
+    'Ami',
+    'Tumi',
+    'ar',
+    'se',
+    'kee',
+    'bolo',
+    'ajbo',
+    'jabo na',
+    'karon',
+    'se',
+    'kotha',
+    'boleni',
+    'tay',
+    'Ami',
+    'Tumi',
+    'ar',
+    'se',
+    'kee',
+    'bolo',
+    'ajbo',
+    'jabo na',
+    'karon',
+    'se',
+    'kotha',
+    'boleni',
+    'tay',
+    'Ami',
+    'Tumi',
+    'ar',
+    'se',
+    'kee',
+    'bolo',
+    'ajbo',
+    'jabo na',
+    'karon',
+    'se',
+    'kotha',
+    'boleni',
+    'tay'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,109 +60,161 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Row and Column'),
-          backgroundColor: Colors.blue.shade200,
+          title: const Text(
+            'List View',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blueGrey,
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                              onPressed: () {}, child: Text('Elevated Button'))
-                        ],
-                      ),
+        // body: ListView(
+        //
+        //   children: [
+        //     SizedBox(
+        //       width: 100,
+        //       height: 100,
+        //       child: Center(
+        //         child: Text('data'),
+        //       )
+        //     ),
+        //     SizedBox(
+        //         width: 100,
+        //         height: 100,
+        //         child: Center(
+        //           child: Text('dfddddddddd'),
+        //         )
+        //     ),
+        //     SizedBox(
+        //         width: 100,
+        //         height: 100,
+        //         child: Center(
+        //           child: Text('fffffffffffff'),
+        //         )
+        //     ),
+        //     SizedBox(
+        //         width: 100,
+        //         height: 100,
+        //         child: Center(
+        //           child: Text('sssssss'),
+        //         )
+        //     ),
+        //     SizedBox(
+        //         width: 100,
+        //         height: 100,
+        //         child: Center(
+        //           child: Text('ffff'),
+        //         )
+        //     ),
+        //     SizedBox(
+        //         width: 100,
+        //         height: 100,
+        //         child: Center(
+        //           child: Text('ads'),
+        //         )
+        //     ),
+        //     SizedBox(
+        //         width: 100,
+        //         height: 100,
+        //         child: Center(
+        //           child: Text('fasdfasdfasdf'),
+        //         )
+        //     ),
+        //     SizedBox(
+        //         width: 100,
+        //         height: 100,
+        //         child: Center(
+        //           child: Text('fasd'),
+        //         )
+        //     ),
+        //     SizedBox(
+        //         width: 100,
+        //         height: 100,
+        //         child: Center(
+        //           child: Text('fasd'),
+        //         )
+        //     ),
+        //     SizedBox(
+        //         width: 100,
+        //         height: 100,
+        //         child: Center(
+        //           child: Text('fasd'),
+        //         )
+        //     ),
+        //     SizedBox(
+        //         width: 100,
+        //         height: 100,
+        //         child: Center(
+        //           child: Text('fasdf'),
+        //         )
+        //     ),
+        //   ],
+        // ),
+        // body: ListView.builder(
+        //     itemCount: 100,
+        //     itemBuilder: (context, index) {
+        //       return SizedBox(
+        //         height: 100,
+        //         width: 100,
+        //         child: Center(
+        //           child: Text('box no ${index.toString()}'),
+        //         ),
+        //       );
+        //     }),
+
+        // body: ListView.builder(
+        //   itemCount: friendList.length,
+        //   itemBuilder: (context, index){
+        //     return Text(friendList[index]);
+        //   },
+        //   scrollDirection: Axis.horizontal,
+        //   padding: EdgeInsets.all(15),
+        //   addRepaintBoundaries: false,
+        // ),
+
+        //This is for just gridview in body with divider and text through line....
+        body: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 5.0,
+            ),
+            itemCount: friendList.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  //Text(index.toString()),
+                  //Text(friendList[index]),
+                  Center(
+                    child: Image.network(
+                      "https://e7.pngegg.com/pngimages/114/142/png-clipart-kylian-mbappe-2018-world-cup-group-c-france-national-football-team-football-player-football-tshirt-hand.png",
+                      fit: BoxFit.cover,
+                      height: 80,
+                      width: 80,
                     ),
-                    SizedBox(
-                      height: 400,
-                    ),
-                    SizedBox(
-                      height: 500,
-                      width: 200,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.max,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            ElevatedButton(
-                                onPressed: () {},
-                                child: Text(
-                                  'SizedBox',
-                                ))
-                          ],
+                  ),
+                  const Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.red,
+                          thickness: 5,
+                          indent: 2.5,
+                          endIndent: 2.5,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                            onPressed: () {}, child: Text('Elevated Button'))
-                      ],
-                    ),
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                            onPressed: () {}, child: Text('Elevated Button'))
-                      ],
-                    ),
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('text 1'),
-                        Text('text 1'),
-                        Text('text 1'),
-                        Text('text 1'),
-                        Text('text 1'),
-                        Text('text 1'),
-                        Text('text 1'),
-                        Text('text 1'),
-                        Text('text 1'),
-                        Text('text 1'),
-                      ],
-                    ),
+                      Text('Line', style: TextStyle(decoration: TextDecoration.lineThrough, decorationColor: Colors.red),),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.red,
+                          thickness: 5,
+                          indent: 2.5,
+                          endIndent: 2.5,
+                        ),
+                      ),
+                    ],
                   )
                 ],
-              ),
-            ],
-          ),
-        ),
+              );
+            }),
       ),
     );
   }
