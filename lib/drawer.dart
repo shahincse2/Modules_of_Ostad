d15338baf70e7b828bb901f8531a49742fcd3038
shahincse2 <shahinalam.cse2@gmail.com>
2024-08-17 12:36:30 +0600

    Drawer, actions with dialog

---
+++ lib/drawer.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+//
+// class Menu extends StatelessWidget {
+//   const Menu({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       drawer: Drawer(
+//         surfaceTintColor: Colors.amber,
+//         child: ListTile(
+//           title: Text('Drawer', style: TextStyle(color: Colors.black),),
+//           leading: Icon(Icons.person),
+//         ),
+//       ),
+//     );
+//   }
+// }
