a68b4571c8dabfc3db016ff548263a1c5af6e1af
shahincse2 <shahinalam.cse2@gmail.com>
2024-10-07 18:44:25 +0600

    Task Management App

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart';
 import 'package:modules_of_ostad/data/services/network_response.dart';
 
 class NetworkCaller {
-  static Future<NetworkResponse> getRequest(String url) async {
+  static Future<NetworkResponse> getRequest({required String url}) async {
     try {
       Uri uri = Uri.parse(url);
       debugPrint(url);
@@ -34,7 +34,7 @@ class NetworkCaller {
   }
 
   static Future<NetworkResponse> postRequest(
-      String url, Map<String, dynamic>? body) async {
+      {required String url, Map<String, dynamic>? body}) async {
     try {
       Uri uri = Uri.parse(url);
       debugPrint(url);
--- lib/ui/screens/sign_up_screen.dart
+++ lib/ui/screens/sign_up_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:modules_of_ostad/data/services/network_caller.dart';
+import 'package:modules_of_ostad/data/utils/urls.dart';
 import 'package:modules_of_ostad/ui/utils/app_colors.dart';
 import 'package:modules_of_ostad/ui/widget/background_screen.dart';
+import 'package:modules_of_ostad/ui/widget/snack_bar_message.dart';
+
+import '../../data/services/network_response.dart';
+import '../widget/centered_circular_progress_indicator.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -11,10 +17,17 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _firsNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final TextEditingController _mobileTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+  bool _inProgress = false;
+
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
-
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: BackgroundScreen(
@@ -52,50 +65,118 @@ class _SignUpScreenState extends State<SignUpScreen> {
   }
 
   Widget buildSignUpForm() {
-    return Column(
-      children: [
-        TextFormField(
-          keyboardType: TextInputType.emailAddress,
-          decoration: const InputDecoration(hintText: 'Email'),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          decoration: const InputDecoration(hintText: 'First name'),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          decoration: const InputDecoration(hintText: 'Last name'),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          keyboardType: TextInputType.number,
-          decoration: const InputDecoration(hintText: 'Mobile'),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          decoration: const InputDecoration(hintText: 'Password'),
-        ),
-        const SizedBox(
-          height: 32,
-        ),
-        ElevatedButton(
-          onPressed: _onTapNextButton,
-          child: const Icon(Icons.arrow_circle_right_outlined),
-        ),
-      ],
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: _emailTEController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: 'Email'),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter your email';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 8),
+          TextFormField(
+            controller: _firsNameTEController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            decoration: const InputDecoration(hintText: 'First name'),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter first name';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 8),
+          TextFormField(
+            controller: _lastNameTEController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            decoration: const InputDecoration(hintText: 'Last name'),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter last name';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 8),
+          TextFormField(
+            controller: _mobileTEController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(hintText: 'Mobile'),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter mobile number';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 8),
+          TextFormField(
+            controller: _passwordTEController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            keyboardType: TextInputType.number,
+            obscureText: true,
+            decoration: const InputDecoration(hintText: 'Password'),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter your password';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 32),
+          Visibility(
+            visible: !_inProgress,
+            replacement: const CenteredCircularProgressIndicator(),
+            child: ElevatedButton(
+              onPressed: _onTapNextButton,
+              child: const Icon(Icons.arrow_circle_right_outlined),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   void _onTapNextButton() {
-    // TODO: Implement on Tap next Button
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    _signup();
+  }
+
+  Future<void> _signup() async {
+    _inProgress = true;
+    setState(() {});
+
+    Map<String, dynamic> requestBody = {
+      "email": _emailTEController.text.trim(),
+      "firstName": _firsNameTEController.text.trim(),
+      "lastName": _lastNameTEController.text.trim(),
+      "mobile": _mobileTEController.text.trim(),
+      "password": _passwordTEController.text,
+      "photo": "",
+    };
+
+    NetworkResponse response = await NetworkCaller.postRequest(
+      url: Urls.registration,
+      body: requestBody,
+    );
+    _inProgress = false;
+    setState(() {});
+    if (response.isSuccess) {
+      SnackBarMessage(context, 'Successfully New User Created!');
+    } else {
+      SnackBarMessage(context, response.errorMessage, true);
+    }
   }
 
   Widget buildHaveAccountSection() {
@@ -123,5 +204,15 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   void _onTapSignInButton() {
     Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _firsNameTEController.dispose();
+    _lastNameTEController.dispose();
+    _mobileTEController.dispose();
+    _passwordTEController.dispose();
+    super.dispose();
   }
 }
